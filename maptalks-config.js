6892b5db9952a2bfdf1f559c7546ace47739f537
Krishna Guru <36165748+KG-11@users.noreply.github.com>
2020-10-26 06:16:11 +0530

    Add files via upload

---
+++ maptalks-config.js
@@ -0,0 +1,6 @@
+const maptalksConfig = {};
+
+maptalksConfig.urlTemplate = 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
+maptalksConfig.subdomains = ['a', 'b', 'c', 'd'];
+maptalksConfig.attribution = '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/">CARTO</a>'
+maptalksConfig.centerCoords = [80.18545, 13.11047];
---
+++ tree-config.js
@@ -0,0 +1,36 @@
+const treeConfig = [{
+        name: 'tree1',
+        coords: [80.18646, 13.10885],
+        musicObj: null,
+        musicUrl: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+        markerHTML: `<div class="hola1"><x3d width='100px' height='80px'> <scene> <inline url="tree.x3d"> </inline> </scene>  </x3d></div>`
+    },
+    {
+        name: 'tree2',
+        coords: [80.18378, 13.10774],
+        musicObj: null,
+        musicUrl: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+        markerHTML: `<div class="hola1"><x3d width='100px' height='80px'> <scene> <inline url="tree.x3d"> </inline> </scene>  </x3d></div>`
+    },
+    {
+        name: 'tree3',
+        coords: [80.18332, 13.10922],
+        musicObj: null,
+        musicUrl: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+        markerHTML: `<div class="hola1"><x3d width='100px' height='80px'> <scene> <inline url="tree.x3d"> </inline> </scene>  </x3d></div>`
+    },
+    {
+        name: 'tree4',
+        coords: [80.18566, 13.11043],
+        musicObj: null,
+        musicUrl: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+        markerHTML: `<div class="hola1"><x3d width='100px' height='80px'> <scene> <inline url="tree.x3d"> </inline> </scene>  </x3d></div>`
+    },
+    {
+        name: 'tree5',
+        coords: [80.18583, 13.10766],
+        musicObj: null,
+        musicUrl: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+        markerHTML: `<div class="hola1"><x3d width='100px' height='80px'> <scene> <inline url="tree.x3d"> </inline> </scene>  </x3d></div>`
+    }
+];
