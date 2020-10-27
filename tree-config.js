805f7854c191c530f17e4b3410d7c57704104f2d
Krishna Guru <36165748+KG-11@users.noreply.github.com>
2020-10-27 20:21:39 +0530

    Update tree-config.js

--- tree-config.js
+++ tree-config.js
@@ -41,7 +41,7 @@ const treeConfig = [{
     {
         name: 'tree6',
         coords: [80.1865728, 13.10360],
-        musicId: 'tree5_audio',
+        musicId: 'tree6_audio',
         allowMusicPlayBack: false,
         musicUrl: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
         markerHTML: `<div class="hola1"><x3d width='100px' height='80px'> <scene> <inline url="tree.x3d"> </inline> </scene>  </x3d></div>`
